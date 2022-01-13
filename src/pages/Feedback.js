9819b5cd5ad1bec61bc5ab7a3cbe85d2ad55d8f1
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-01-13 15:48:22 +0200

    feedback page completed

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,51 @@
 import React from "react";
-import { useAppContext } from "../context/app_context";
+import { useForm, ValidationError } from "@formspree/react";
+import Button from "../components/Button";
 
 const Feedback = () => {
-  const value = useAppContext();
+  const [state, handleSubmit] = useForm("mdobakzb");
+  if (state.succeeded) {
+    return (
+      <section className="feedback-page container section-p page-min-height">
+        <h3>Thanks for your feedback!</h3>
+      </section>
+    );
+  }
+
   return (
     <section className="feedback-page container section-p page-min-height">
-      <h1>Feedback page</h1>
+      <form className="feedback-form" onSubmit={handleSubmit}>
+        <div className="form-input">
+          <label htmlFor="name" className="sb">
+            Name
+          </label>
+          <input type="text" name="name" id="name" required />
+          <ValidationError
+            prefix="Message"
+            field="name"
+            errors={state.errors}
+          />
+        </div>
+        <div className="form-input">
+          <label htmlFor="email" className="sb">
+            Email
+          </label>
+          <input type="email" name="email" id="email" required />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div className="form-input">
+          <label htmlFor="feedback" className="sb">
+            Feedback
+          </label>
+          <textarea name="feedback" id="feedback" required></textarea>
+          <ValidationError
+            prefix="Message"
+            field="feedback"
+            errors={state.errors}
+          />
+        </div>
+        <Button className="w-100">Submit</Button>
+      </form>
     </section>
   );
 };
